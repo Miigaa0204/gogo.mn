396bf08214eb6a8d2d6112aa54d67e5078568cf5
24LP2769 24LP2769 <mytawochir@gmail.com>
2024-06-05 18:38:03 +0800

    new project

---
+++ variable.js
@@ -0,0 +1,23 @@
+    /*  let var1 = 4  ,var2 = 3 , var3 = 10 , var4 = 12
+        result = (var1 > var2 ) && (var3 < var4);
+        console.log(result)  */
+        
+    /*  let var1 = 4  ,var2 = 3 , var3 = 10 , var4 = 12
+        result = (var1 > var2 ) && (var3 > var4);
+        console.log(result)  */
+    
+    /*  let var1 = 4  ,var2 = 3
+        let var3 = 10 , var4 = 12
+        result = (var1 > var2 )
+        result2 = (var3 < var4)
+        console.log(result , result2)  */
+
+    /*  let var1 = 4  ,var2 = 3
+        let var3 = 10 , var4 = 12
+        result = (var1 > var2 )
+        result2 = (var3 > var4)
+        console.log(result , result2)  */
+
+    let year = prompt()
+    let month = prompt()
+    if((year % 4 == 0 && year % 100 !=0) || (year % 400 = 0))
