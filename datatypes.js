ddb749f53321bb91382a8a4dee55e4575279ccce
Miigaa <mytawochir@gmail.com>
2024-07-05 17:20:54 +0800

    miigaa-todo-week

---
+++ datatypes.js
@@ -0,0 +1,5 @@
+let sum = 10;
+for (let n = 0; n <= 20; n= n=2) {
+  sum = sum + n;
+}sum
+console.log(sum);
--- variable.js
+++ variable.js
@@ -1,23 +1,72 @@
-    /*  let var1 = 4  ,var2 = 3 , var3 = 10 , var4 = 12
+/*  let var1 = 4  ,var2 = 3 , var3 = 10 , var4 = 12
         result = (var1 > var2 ) && (var3 < var4);
         console.log(result)  */
-        
-    /*  let var1 = 4  ,var2 = 3 , var3 = 10 , var4 = 12
+
+/*  let var1 = 4  ,var2 = 3 , var3 = 10 , var4 = 12
         result = (var1 > var2 ) && (var3 > var4);
         console.log(result)  */
-    
-    /*  let var1 = 4  ,var2 = 3
+
+/*  let var1 = 4  ,var2 = 3
         let var3 = 10 , var4 = 12
         result = (var1 > var2 )
         result2 = (var3 < var4)
         console.log(result , result2)  */
 
-    /*  let var1 = 4  ,var2 = 3
+/*  let var1 = 4  ,var2 = 3
         let var3 = 10 , var4 = 12
         result = (var1 > var2 )
         result2 = (var3 > var4)
         console.log(result , result2)  */
 
-    let year = prompt()
-    let month = prompt()
-    if((year % 4 == 0 && year % 100 !=0) || (year % 400 = 0))
+/*let A = 4 ,B = 3
+        if( A > B){
+            console.log("A")
+        } else{console.log("B")}*/
+
+/*let A = 4 ,B = 3 , C = 5
+        if( A > B && A > C){
+            console.log("A")
+        }else if(B > A && B > C ){
+            console.log( "B")
+        } else{
+            console.log("C")
+        } */
+
+/*let A=5 , B=7 , C=9 , D=2
+        if( A > B && A > C && A < D){
+            console.log("A")
+        }else if(B > A && B > C && B < D){
+            console.log( "B")
+        } else if(C > A && C > B && C < D){
+            console.log("C")
+        }else if( D > A && D > B && D < C){
+            console.log("D")
+        }*/
+
+/*let day = prompt()
+     if( day=1 ,2,3,4,5,6,7){
+        console.log(1="Monday")
+        console.log(2="Tuesday")
+        console.log(3="Wednesday")
+        console.log(4="Thursday")
+        console.log(5="Friday")
+        console.log(6="Saturday")
+        console.log(7="Sunday")
+     }else{
+        console.log("Таны оруулсан утга алдаатай байна..")
+     }*/
+
+//for loop
+
+/*let sum = 0;
+for (let n = 10; n <= 20; n=n+2) {
+  sum = sum + n;
+}
+console.log(sum);*/
+
+/*let sum = 10;
+for (let n = 0; n <= 20; n= n+2) {
+  sum = sum + n;
+}sum
+console.log(sum);
+*/
